32599d82651d9c047161e0992914357d66875e4e
Anna <anna.lukash68@gmail.com>
2020-05-18 14:35:52 +0300

    created a class that dynamically adds elements

--- js/index.js
+++ js/index.js
@@ -129,7 +129,7 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = '';
     }
 
-    const modalTimerid = setTimeout(openModal, 5000);
+    // const modalTimerid = setTimeout(openModal, 5000);
 
     function showModalByScrool() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.
@@ -140,5 +140,68 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     window.addEventListener('scroll', showModalByScrool);
+
+    class Menu {
+        constructor(src, alt, title, description, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.description = description;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">Меню ${this.title}</h3>
+                <div class="menu__item-descr">${this.description}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    const FitnessMenu = new Menu(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        "Фитнес",
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container');
+
+    const PremiumMenu = new Menu(
+        "img/tabs/elite.jpg",
+        "elite",
+        "Премиум",
+        "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+        15,
+        '.menu .container');
+
+    const PostMenu = new Menu(
+        "img/tabs/post.jpg",
+        "post",
+        "Постное",
+        "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+        10,
+        '.menu .container');
+    
+        FitnessMenu.render();
+        PremiumMenu.render();
+        PostMenu.render();
+
 
 });
